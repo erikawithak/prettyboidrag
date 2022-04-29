7c3eca374cd11a1787ed04f7614e9ac8169197c0
Erika Lewis <me.erikawithak@gmail.com>
2022-04-29 15:44:03 -0400

    remove JS on button

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,16 @@
-(function ($) {
-	$("button.form-trigger").on("click", function () {
-		$("form.contact-form").css("display", "flex");
-	});
+// (function ($) {
+// 	$("button.form-trigger").on("click", function () {
+// 		$("form.contact-form").css("display", "flex");
+// 	});
 
-	$("form.contact-form .close").on("click", function () {
-		$(this).parent().hide();
-	});
+// 	$("form.contact-form .close").on("click", function () {
+// 		$(this).parent().hide();
+// 	});
 
-	window.onload = function () {
-		var el = document.getElementById("g-recaptcha-response");
-		if (el) {
-			el.setAttribute("required", "required");
-		}
-	};
-})(jQuery);
+// 	window.onload = function () {
+// 		var el = document.getElementById("g-recaptcha-response");
+// 		if (el) {
+// 			el.setAttribute("required", "required");
+// 		}
+// 	};
+// })(jQuery);
